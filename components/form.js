ca8cf26a710e64cf703dbf6deed186f96a82cb18
MicahJank <MicahJank@users.noreply.github.com>
2019-06-27 13:58:02 -0500

    added classes that extend the Form class

--- components/form.js
+++ components/form.js
@@ -1,12 +1,12 @@
+// The form class is responsible for creating the neccesary classes inside of a form
 class Form {
-    constructor(element) {
-        this.element = element;
+    constructor(formElement) {
+        this.element = formElement;
 
-        this.labels = this.element.querySelectorAll('label');
-
-        this.inputs = this.element.querySelectorAll('input');
-
-        this.textAreas = this.element.querySelectorAll('textarea');
+        // Creating the children classes
+        this.labels = this.element.querySelectorAll('label').forEach(label => new Label(label));
+        this.inputs = this.element.querySelectorAll('input').forEach(input => new Input(input));
+        this.textAreas = this.element.querySelectorAll('textarea').forEach(texarea => new TextArea(texarea));
 
         // In this case my buttons are an input, so what i need to do is filter through
         // the inputs that i have already found and only return the ones with a type
@@ -18,10 +18,42 @@ class Form {
     }
 }
 
-class Button {
-    constructor(element) {
-        this.element = element;
+// LABEL
+class Label extends Form {
+    constructor(labelElement) {
+        super(labelElement);
+
+        this.element = labelElement;
     }
+}
+
+// INPUT
+class Input extends Form {
+    constructor(inputElement) {
+        super(inputElement);
+
+        this.element = inputElement;
+    }
+}
+
+// TEXT AREA
+class TextArea extends Form {
+    constructor(textAreaElement) {
+        super(textAreaElement);
+
+        this.element = textAreaElement;
+    }
+}
+
+// BUTTON
+class Button extends Form {
+    constructor(buttonElement) {
+        super(buttonElement);
+
+        this.element = buttonElement;
+    }
+
+
 }
 
 
