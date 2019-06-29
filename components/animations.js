a27e83083576b80bca6a3a45d0e5fcb502df845b
MicahJank <MicahJank@users.noreply.github.com>
2019-06-29 15:51:25 -0500

    did more animations for homepage header

--- components/animations.js
+++ components/animations.js
@@ -1,11 +1,9 @@
-
-
 const animateElements = () => {
 
     const headerElements = document.querySelector('#introduction .inner-text');
     const navElements = document.querySelector('.dropdown-content');
     const iphoneElement = document.querySelector('.iphone-section img');
-
+    const iphoneText = document.querySelector('.bottom-side');
     // headerElements
     TweenMax.fromTo(headerElements, 2, {
         opacity: 0,
@@ -25,14 +23,24 @@ const animateElements = () => {
         delay: 1
     });
 
-     // iphoneImg
-     TweenMax.fromTo(iphoneElement, 2, {
+    // iphoneImg
+    TweenMax.fromTo(iphoneElement, 2, {
         opacity: 0,
         left: 50
     }, {
         opacity: 1,
         left: 0,
         delay: 2
+    });
+
+    // iphoneText
+    TweenMax.fromTo(iphoneText, 2, {
+        opacity: 0,
+        bottom: 60
+    }, {
+        opacity: 1,
+        bottom: 0,
+        delay: 3.5
     });
 
 };
